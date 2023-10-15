b24eaeb21f7e9854e04937f60a8678f2dfd9874d
ManuelSpinola <mspinola10@gmail.com>
2023-10-14 20:24:06 -0600

    2nd committ

--- data-raw/ca_hex_grids.R
+++ data-raw/ca_hex_grids.R
@@ -1,3 +1,22 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare the ca hex grid data
 
-usethis::use_data(DATASET, overwrite = TRUE)
+library(sf)
+library(here)
+
+ca_hex_grid_res_5 <- st_read(here("data-raw/ca_hexagonal_grids/ca_hex_res_5.gpkg"))
+
+usethis::use_data(ca_hex_grid_res_5, overwrite = TRUE)
+
+ca_hex_grid_res_6 <- st_read(here("data-raw/ca_hexagonal_grids/ca_hex_res_6.gpkg"))
+
+usethis::use_data(ca_hex_grid_res_6, overwrite = TRUE)
+
+ca_hex_grid_res_7 <- st_read(here("data-raw/ca_hexagonal_grids/ca_hex_res_7.gpkg"))
+
+usethis::use_data(ca_hex_grid_res_7, overwrite = TRUE)
+
+ca_hex_grid_res_8 <- st_read(here("data-raw/ca_hexagonal_grids/ca_hex_res_8.gpkg"))
+
+usethis::use_data(ca_hex_grid_res_8, overwrite = TRUE)
+
+
